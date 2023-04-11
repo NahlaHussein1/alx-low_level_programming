eb6d5be0e41f10369dd87e8c1ed17320d79ee718
NahlaHussein1 <nahlahussein518@gmail.com>
2023-04-10 17:23:20 -0700

    d

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,28 +1,25 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-int main(void)
-{
-        unsigned long int num;
-        unsigned long int x;
-        unsigned long int y;
-        unsigned long int z;
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
-        x = 1;
-        y = 2;
-        printf("%d, ", x);
-        printf("%d, ", y);
-        for (num = 3; num < 51; num++)
-        {
-                z = y;
-                y += x;
-                printf("%d", y);
-                if (num == 50)
-                {
-                        break;
-                }
-                printf(", ");
-                x = z;
-        }
-        printf("\n");
-        return (0);
-}
+#endif
